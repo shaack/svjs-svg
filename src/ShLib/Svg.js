55bd04140e1a3f169df40914aa6d7e28b21894d6
shaack <shaack@gmail.com>
2017-11-24 00:02:20 +0100

    removed comments

--- src/ShLib/Svg.js
+++ src/ShLib/Svg.js
@@ -3,8 +3,6 @@
  * Date: 23.11.2017
  */
 
-// https://stackoverflow.com/questions/16488884/add-svg-element-to-existing-svg-using-dom
-
 const NAMESPACE = "http://www.w3.org/2000/svg";
 
 export class Svg {
